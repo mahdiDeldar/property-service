88949c8a89ac5c0a1107af17d39b98304e4c2314
Mohammad-PC <en.masoomi@gmail.com>
2022-07-06 01:06:58 +0430

    BUG: commenting V10 report table update

--- src/main/resources/db/migration/V10__UPDATE_REPORTS.sql
+++ src/main/resources/db/migration/V10__UPDATE_REPORTS.sql
@@ -1,3 +1,3 @@
-alter table property_service.reports add column property_id varchar(64);
-alter table property_service.reports rename column text to cause;
-alter table property_service.reports add column text text;
+-- alter table property_service.reports add column property_id varchar(64);
+-- alter table property_service.reports rename column text to cause;
+-- alter table property_service.reports add column text text;
